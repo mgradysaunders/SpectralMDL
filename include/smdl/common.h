a77d7503ce22a8c2467e1e2de91e41c9d1100a11
grady <mgscis@cis.rit.edu>
2025-06-17 16:45:35 -0400

    Add documentation comments, remove empty filesystem source file.

--- include/smdl/common.h
+++ include/smdl/common.h
@@ -664,25 +664,48 @@ public:
 /// \name Filesystem
 /// \{
 
+/// Exists?
 [[nodiscard]] SMDL_EXPORT bool exists(const std::string &path) noexcept;
 
+/// Is file?
 [[nodiscard]] SMDL_EXPORT bool is_file(const std::string &path) noexcept;
 
+/// Is directory?
 [[nodiscard]] SMDL_EXPORT bool is_directory(const std::string &path) noexcept;
 
+/// Make path canonical.
+///
+/// \note
+/// This does not throw. If the implementation fails for any reason, the input
+/// path is returned unchanged.
+///
 [[nodiscard]]
 SMDL_EXPORT std::string canonical(std::string path) noexcept;
 
+/// Make path relative to working directory.
+///
+/// \note
+/// This does not throw. If the implementation fails for any reason, the input
+/// path is returned unchanged.
+///
 [[nodiscard]]
 SMDL_EXPORT std::string relative(std::string path) noexcept;
 
+/// Determine parent path.
+///
+/// \note
+/// This does not throw. If the implementation fails for any reason, the input
+/// path is returned unchanged.
+///
 [[nodiscard]]
 SMDL_EXPORT std::string parent_path(std::string path) noexcept;
 
+/// Open file or throw an `Error`.
 [[nodiscard]]
 SMDL_EXPORT std::fstream open_or_throw(const std::string &path,
-                                            std::ios::openmode mode);
+                                       std::ios::openmode mode);
 
+/// Read file or throw an `Error`.
 [[nodiscard]]
 SMDL_EXPORT std::string read_or_throw(const std::string &path);
 
