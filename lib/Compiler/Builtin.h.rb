6dc0bffe7e776b081a8ae1291d7cd3cb9cc4c233
grady <mgscis@cis.rit.edu>
2026-07-30 13:36:28 -0400

    Upgrade to more recent energy-conserving Oren Nayar and sheen BRDFs, and add support for improved sampling technique for GGX BRDF-only use cases.

--- lib/Compiler/Builtin.h.rb
+++ lib/Compiler/Builtin.h.rb
@@ -45,7 +45,7 @@ f.write <<STR
 }
 STR
 
-fnames = ['diffuse_reflection_bsdf', 'microfacet_ggx_smith_bsdf', 'microfacet_beckmann_smith_bsdf', 'sheen_bsdf', 'simple_glossy_bsdf', 'ward_geisler_moroder_bsdf']
+fnames = ['microfacet_ggx_smith_bsdf', 'microfacet_beckmann_smith_bsdf', 'simple_glossy_bsdf', 'ward_geisler_moroder_bsdf']
 for fname in fnames
   f.write "#include \"Builtin/Albedo/#{fname}.inl\"\n"
 end
